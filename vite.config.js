25b81d270529ce0a64c4e9bac2eae54cad90c94b
Metin <misaxanli@gmail.com>
2023-07-29 08:33:33 +0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/User-Authentication-Firebase-ReactJS/'
+  base:'/User-Atuh-Firebase-ReactJS/'
 })
